280ad3b4202e80eff24e247da19a0f0be9dac82d
shilewenuw <shilewen1@gmail.com>
2023-03-10 11:26:57 -0800

    add draw.sh

---
+++ draw.sh
@@ -0,0 +1,11 @@
+xsetwacom --set "Tablet Monitor Pad pad" Button 1 "key +ctrl +z -z -ctrl"
+xsetwacom --set "Tablet Monitor Pad pad" Button 2 "key +ctrl +shift +z -ctrl -shift -z"
+xsetwacom --set "Tablet Monitor Pad pad" Button 3 "key +ctrl +space"
+xsetwacom --set "Tablet Monitor Pad pad" Button 8 "key +shift +space"
+
+xsetwacom --set "Tablet Monitor Pad pad" Button 9 "key +shift +BackSpace -shift -BackSpace"
+xsetwacom --set "Tablet Monitor Pad pad" Button 10 "key x"
+xsetwacom --set "Tablet Monitor Pad pad" Button 11 "key +shift"
+xsetwacom --set "Tablet Monitor Pad pad" Button 12 "key +ctrl +shift +a -ctrl -shift -a"
+xsetwacom --set "Tablet Monitor Pen stylus" Button 3 3
+
